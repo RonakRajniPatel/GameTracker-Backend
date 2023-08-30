239d7a03210dc1146591d1aa8a2ed901457b0afd
ronakpatel <ronakrajnipatel@gmail.com>
2023-08-29 21:54:47 -0400

    added a session

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 var router = express.Router();``
 var PORT = 8080;
+const session = require('express-session');
 
 // importing routes
 const {fetchGames, fetchToken} = require('./routes/igdbRoutes');
@@ -16,6 +17,25 @@ app.use(cors());
 app.use('/', frontendRoutes);
 
 gameList = [];
+
+// creates a session
+app.use(session({
+    secret: 'generated_key', // change later, created issue
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+// testing the session
+app.get('/', (req, res) => {
+    if (req.session.views) {
+        req.session.views++;
+        res.send(`<h1>Visits: ${req.session.views}`);
+    } else {
+        req.session.views = 1;
+        res.send('Welcome to this website. You are visiting for the first time!');
+    }
+});
 
 // listener
 const server = app.listen(PORT, () => {
---
+++ routes/authRoutes.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
